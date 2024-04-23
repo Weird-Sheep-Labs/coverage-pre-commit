343c6aa2a3cf3b4fc0b459e9d0600317f66d1965
Armand Rego <armand@weirdsheeplabs.com>
2024-04-23 08:01:56 +0100

    Handle default input/output file locations better and add comments.

--- coverage-badge.sh
+++ coverage-badge.sh
@@ -1,18 +1,29 @@
 #!/bin/bash
 
+# pre-commit doesn't hook into user's venv so need to install required packages 
 pip install genbadge[coverage]==1.1.1 > /dev/null
 
-outfile=coverage-badge.svg
-while getopts o: flag
+# Specify default file locations but replace with user-specified overrides if present
+infile=reports/coverage/coverage.xml
+outfile=reports/coverage/coverage-badge.svg
+while getopts i:o: flag
 do
     case "${flag}" in
+        i) infile=${OPTARG};;
         o) outfile=${OPTARG};;
     esac
 done
 
-genbadge coverage $@
-sed -i "" "s/timestamp=\"[0-9]*\"//g" $outfile
+# Check input file location is valid; raise error if not
+if ! test -f $infile; then
+  echo "Could not find coverage XML file: $infile"
+  exit 1
+fi
 
+# Override default file locations and generate badge
+genbadge coverage $@ -i $infile -o $outfile
+
+# Exits non-zero if generated file is modified or untracked (initial creation)
 if git status -s | awk '/[AM ]M / { print $2 }' | 
     grep --quiet --fixed-strings $outfile 
 then    
--- coverage-xml.sh
+++ coverage-xml.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
+# pre-commit doesn't hook into user's venv so need to install required packages 
 pip install coverage==7.4.4 > /dev/null
 
+# Specify default output file location but replace with user-specified override if present
 outfile=reports/coverage/coverage.xml
 while getopts o: flag
 do
@@ -10,9 +12,13 @@ do
     esac
 done
 
-coverage xml $@
+# Override default output file location and generate coverage report
+coverage xml $@ -o $outfile
+
+# Remove timestamp from output file to avoid unnecessary diffs
 sed -i "" "s/timestamp=\"[0-9]*\"//g" $outfile
 
+# Exits non-zero if generated file is modified or untracked (initial creation)
 if git status -s | awk '/[AM ]M / { print $2 }' | 
     grep --quiet --fixed-strings $outfile 
 then    
